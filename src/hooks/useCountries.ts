070108d8b3af08b2848bc1b21314187128a96e20
Alejandro Hortal Valor <alejandrohortalvalor02@gmail.com>
2024-12-12 00:08:33 +0100

    Hooks done

---
+++ src/hooks/useCountries.ts
@@ -0,0 +1,44 @@
+// Este es un ejemplo de cómo podrías estructurar tu hook useCountries
+import {useState, useEffect} from 'react';
+import {CountryDetails} from '../types/CountryDetails';
+import {CountryRequest} from '../types/CountryRequest';
+
+export const useCountries = (continent: string) => {
+  const [countries, setCountries] = useState<CountryDetails[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch(
+          `https://restcountries.com/v3.1/region/${continent}`,
+        );
+        const data = await response.json();
+
+        const formattedCountries: CountryDetails[] = data.map(
+          (item: CountryRequest) => ({
+            name: item.name,
+            cca3: item.cca3,
+            region: item.region,
+            subregion: item.subregion ?? 'N/A',
+            population: item.population,
+            capital: item.capital,
+            languages: item.languages,
+            flags: item.flags.png,
+          }),
+        );
+
+        setCountries(formattedCountries);
+      } catch (err) {
+        setError('Error al cargar países');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCountries();
+  }, [continent]);
+
+  return {countries, loading, error};
+};
---
+++ src/hooks/useDataCountry.ts
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react';
+import {HttpAxios} from '../mapper/http/HttpAxios';
+import {HttpError} from '../mapper/http/HttpError';
+import {CountryDetails} from '../types/CountryDetails';
+
+export const useCountryByName = (countryName: string) => {
+  const [country, setCountry] = useState<CountryDetails | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const url_base = 'https://restcountries.com/v3.1/';
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      const httpClient = new HttpAxios({url_base});
+      try {
+        const response = await httpClient.getCountryByName(countryName);
+        if (response instanceof HttpError) {
+          setError('Error fetching country data');
+        } else {
+          setCountry(response[0]);
+        }
+      } catch (err) {
+        setError(err instanceof Error ? err.message : 'Unknown error');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (countryName) {
+      fetchCountry();
+    }
+  }, [countryName]);
+
+  return {country, loading, error};
+};
